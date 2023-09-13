545817067ed985cdc464e289ed8bd82502e40a46
Vidya Sinta Billkis <vidya.sinta21@students.unila.ac.id>
2023-09-13 15:33:20 +0700

    Ubah Routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Controllers\Home;
 use CodeIgniter\Router\RouteCollection;
 
 /**
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+$routes->get('/profile', 'Home::profile');
+
+$routes->get('/profile/(:any)/(:any)/(:any)', [Home::class, 'profile']);
