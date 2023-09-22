a750c35f8cfd38b23fe9861c3432c19c67df6b75
Vidya Sinta Billkis <vidya.sinta21@students.unila.ac.id>
2023-09-22 09:10:17 +0700

    Database Model

--- app/Views/profile.php
+++ app/Views/profile.php
@@ -3,17 +3,24 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Profile</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="<?= base_url("assets/css/style.css")?>">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;800;900&display=swap"rel="stylesheet">
+</head>
 </head>
 <body>
-    <br><br><br><h1><center>Profile Diri</center></h1><br>
-    <div style="text-align:center;">
-        <img src="<?php echo base_url('assets/img/pidi.jpg'); ?>" alt="pidi" style="height: 200px; width: 200px; border-radius: 50%; object-fit: cover;" ><br><br><br>
-        <button type="button" class="btn btn-warning" style="width: 225px;"><?= $nama ?></button><br><br>
-        <button type="button" class="btn btn-warning" style="width: 225px;"><?= $kelas ?></button><br><br>
-        <button type="button" class="btn btn-warning" style="width: 225px;"><?= $npm ?></button><br><br>
+    <div class="card">
+        <img src="<?php echo base_url('assets/img/pidi.jpg'); ?>" alt="pidi" class="img">
+        <br><br><h3 class="name"><?= $nama ?></h3>
+        <p class="npm"><?= $npm ?><br><?= $id_kelas ?></p>
+        <div class="desc">
+            <p>
+                "Aku emang suka gajelas, tapi kalo suka kamu itu udah jelas"
+            </p>
+        </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
